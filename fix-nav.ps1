904f860a0666fc1fd2e14c08c39fa597fddc48dd
kalebwolfgang <kalebbeiter@gmail.com>
2026-05-10 20:39:06 -0400

    Add calendar to nav

--- fix-nav.ps1
+++ fix-nav.ps1
@@ -8,12 +8,16 @@ foreach ($f in $files) {
     $c = $c.Replace('Meeting Calendar', 'Calendar')
     if ($c -notmatch 'href="calendar\.html"') {
         $c = $c.Replace(
-            '<a class="nav-link" href="index.html">All Boards</a>',
-            '<a class="nav-link" href="calendar.html">Calendar</a>' + "`r`n            " + '<a class="nav-link" href="index.html">All Boards</a>'
+            '<a class="nav-link" href="calendar.html">Calendar</a>
+            <a class="nav-link" href="index.html">All Boards</a>',
+            '<a class="nav-link" href="calendar.html">Calendar</a>' + "`r`n            " + '<a class="nav-link" href="calendar.html">Calendar</a>
+            <a class="nav-link" href="index.html">All Boards</a>'
         )
         $c = $c.Replace(
-            '<a class="nav-secondary-link" href="index.html">All Boards</a>',
-            '<a class="nav-secondary-link" href="calendar.html">Calendar</a>' + "`r`n        " + '<a class="nav-secondary-link" href="index.html">All Boards</a>'
+            '<a class="nav-secondary-link" href="calendar.html">Calendar</a>
+        <a class="nav-secondary-link" href="index.html">All Boards</a>',
+            '<a class="nav-secondary-link" href="calendar.html">Calendar</a>' + "`r`n        " + '<a class="nav-secondary-link" href="calendar.html">Calendar</a>
+        <a class="nav-secondary-link" href="index.html">All Boards</a>'
         )
     }
     [System.IO.File]::WriteAllText($f.FullName, $c, $enc)
